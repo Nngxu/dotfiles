bfac8044578109a62be513b1faeacb072fcd8aef
Xu Ning <ningxu2020@gmail.co>
2022-12-22 17:49:18 +0800

    "Add i3 config"

--- install.sh
+++ install.sh
@@ -8,5 +8,15 @@ mkdir -p "$HOME/.config/nvim/undo"
 
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 
+#######
+# X11 #
+#######
 rm -rf "$HOME/.config/X11"
 ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+
+
+######
+# i3 #
+######
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
